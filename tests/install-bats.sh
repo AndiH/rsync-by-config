42ea532eb89a022d2e1d26fe7e724751f8b95138
Andreas Herten <a.herten@fz-juelich.de>
2017-01-09 17:03:04 +0100

    Add helper tools

--- tests/install-bats.sh
+++ tests/install-bats.sh
@@ -5,3 +5,4 @@ set -o xtrace
 
 git clone --depth 1 https://github.com/sstephenson/bats
 cd bats && ./install.sh "${HOME}/.local" && cd .. && rm -rf bats
+git clone https://github.com/ztombol/bats-support test/test_helper/bats-support
--- tests/test_common.bash
+++ tests/test_common.bash
@@ -1,8 +1,18 @@
 #!/usr/bin/env bats
 
-TEST_BREW_PREFIX="$(brew --prefix)"
-load "${TEST_BREW_PREFIX}/lib/bats-support/load.bash"
-load "${TEST_BREW_PREFIX}/lib/bats-assert/load.bash"
+if [ "$(uname)" == "Darwin" ]; then
+    SYSTEM="MAC"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    SYSTEM="LINUX"
+fi
+
+if [[ $SYSTEM == "MAC" ]]; then
+	PREFIX="$(brew --prefix)/lib"
+elif [[ $SYSTEM == "LINUX" ]]; then
+	RPREFIX="test/test_helper"
+fi
+load "${PREFIX}/bats-support/load.bash"
+load "${PREFIX}/bats-assert/load.bash"
 
 function cleanRemote() {
 	ssh coba "rm ~/tests/rbc/*"
